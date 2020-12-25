adf8c6285664c64fcd9250543967210046e079eb
VELAMPATI <sailaja.velampati@capgemini.com>
2020-12-25 15:11:07 +0530

    back button.

--- src/components/hearder.js
+++ src/components/hearder.js
@@ -5,20 +5,27 @@ import Typography from "@material-ui/core/Typography";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import HomeStyle from "../pages/HomeStyle";
 import { useHistory } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+
 const useStyles = makeStyles((theme) => HomeStyle(theme, fade));
 
 const Layout = (props) => {
   const classes = useStyles();
   const history = useHistory();
+  console.log(props.location);
   return (
     <>
       <AppBar position="static" className={classes.AppBar}>
         <Toolbar onClick={() => history.push(`/`)} data-testid="toolBar">
+          {props.location.pathname !== "/" ? <ArrowBackIosIcon /> : null}
+
           <Typography
             className={classes.title}
             variant="h6"
             noWrap
             data-testid="AppTitle"
+            style={{ verticalAlign: "center" }}
           >
             SHOWS
           </Typography>
@@ -29,4 +36,4 @@ const Layout = (props) => {
   );
 };
 
-export default Layout;
+export default withRouter(Layout);
